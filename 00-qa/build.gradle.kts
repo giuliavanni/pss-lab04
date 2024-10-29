9b4fd40bcd14dde88a9b96937312b2a452fe5b93
giuliavanni <giulia.vanni6@studio.unibo.it>
2024-10-29 10:00:07 +0100

    add chackstyle in build file

--- 00-qa/build.gradle.kts
+++ 00-qa/build.gradle.kts
@@ -1,1 +1,16 @@
-plugins { java }
+plugins {
+    id 'java'
+    id 'checkstyle'
+}
+
+checkstyle {
+    toolVersion = '8.45'
+    configFile = file('checkstyle.xml')
+}
+
+tasks.withType(Checkstyle) {
+    reports {
+        xml.enabled = false
+        html.enabled = true
+    }
+}
