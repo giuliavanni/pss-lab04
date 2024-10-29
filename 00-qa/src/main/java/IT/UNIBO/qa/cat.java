16a0e489a6c6bf1b41fb865a28b8a61e8c1d78f3
giuliavanni <giulia.vanni6@studio.unibo.it>
2024-10-29 09:46:44 +0100

    code changes 1

--- 00-qa/src/main/java/IT/UNIBO/qa/cat.java
+++ 00-qa/src/main/java/IT/UNIBO/qa/cat.java
@@ -3,10 +3,6 @@ package IT.UNIBO.qa;
 public class cat {
 //this class describes a cat
 
-    
-
-
-
 public String Name, Color, Breed;
 public int Age;
 
@@ -14,26 +10,22 @@ public int Age;
 private static int  NINE = 9;
 static public  int numberoflives= NINE;
 
-public cat(String meow,
-final String c,final int age,
-final String korat) {
-    this.Name =meow ;
-    Color=c  ;
+public cat(String meow, final String c, final int age, final String korat) {
+    this.Name =meow;
+    Color=c ;
     this.Age=age;
     Breed= korat;
 }
 
 // This method is useful because it allows the cat to meow and introduce itself to the world in a single line of code.
-    public String
-    Meow()
-    {return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
+    public String Meow() {
+    return "Meow, meow, I am " + Name + ", hear me purr, In this feline world, I'm the star, that's for sure. With whiskers long and fur so sleek, I'm here to share my story, so take a peek!";
 }public
 
 
 
 void setcolor(String c) {
-    this.Color = 
-    c;
+    this.Color = c;
 }
 
     public String GET_COLOR() {
